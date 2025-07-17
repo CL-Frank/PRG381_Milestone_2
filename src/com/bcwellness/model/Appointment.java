87cd8ab2a63b2cdc94f8cc0907b363a5a38a7497
ygdaarchitect <yangamazibuko154@gmail.com>
2025-07-17 17:18:53 +0200

    Edited the model files
    Add Appointment model for scheduling data
    Implemented Appointment class with counselor, date, time, and status fields
    Part of com.bcwellness.model package for MVC structure
    Add Counselor model for staff data
    Implemented Counselor class with name, specialization, and availability fields
    Part of com.bcwellness.model package for MVC structure
    Add Feedback model for user ratings
    Implemented Feedback class with rating and comments fields
    Part of com.bcwellness.model package for MVC structure

--- src/com/bcwellness/model/Appointment.java
+++ src/com/bcwellness/model/Appointment.java
@@ -9,5 +9,48 @@ package com.bcwellness.model;
  * @author USER-PC
  */
 public class Appointment {
-    
+    private String counselor;
+    private String date;
+    private String time;
+    private String status;
+
+    public Appointment(String counselor, String date, String time, String status) {
+        this.counselor = counselor;
+        this.date = date;
+        this.time = time;
+        this.status = status;
+    }
+
+    public String getCounselor() {
+        return counselor;
+    }
+
+    public void setCounselor(String counselor) {
+        this.counselor = counselor;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
+
--- src/com/bcwellness/model/Feedback.java
+++ src/com/bcwellness/model/Feedback.java
@@ -9,5 +9,27 @@ package com.bcwellness.model;
  * @author USER-PC
  */
 public class Feedback {
-    
+    private String rating;
+    private String comments;
+
+    public Feedback(String rating, String comments) {
+        this.rating = rating;
+        this.comments = comments;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
 }
